b67fb1a45299b0d8c8389b997c9065332669bf10
lopezrnl <ranielcarllopez13@gmail.com>
2024-11-20 16:17:10 +0800

    Add files via upload
    DBMS Activity-Sari Sari Store

---
+++ Lopez_Raniel_IT 211SariSariStore.sql
@@ -0,0 +1,43 @@
+/*create database dbsss;*/
+
+/*create table sarisaristore(
+	ProductID int primary key auto_increment,
+    Name varchar (50) not null,
+    Type varchar (50) not null,
+    Price varchar (15) not null,
+    StockQuantity int (4)
+);*/
+
+insert into sarisaristore (
+Name,
+Type,
+Price,
+StockQuantity
+) values
+("Marlboro Red", "Cigarette", "Php.200.00", 9),
+("Clear Blue", "Shampoo", "Php.8.00", 25),
+("Siga Mang. Tomas", "Sauce", "Php.38.00", 13),
+("Mentos", "Candy", "Php.1.50", 59),
+("Snacku", "Snack", "Php.7.00", 12),
+("Big Boy", "Snack", "Php.8.00", 20),
+("Sampalok", "Candy", "Php.1.50", 59),
+("Joy Yellow", "Dishwashing Liquid", "Php.7.00", 32),
+("Dueberry", "Biscuit", "Php.6.00", 30),
+("555 Afritada", "Canned Foods", "Php.29.00", 15),
+("Fortune White", "Cigarette", "Php.100.00", 14),
+("Knorr Cubes", "Condiments", "Php.7.00", 19),
+("Cornetto", "Gelatto", "Php.25.00", 2),
+("Bubble Joe", "Gum", "Php.1.00", 82),
+("Hi-ho", "Snack", "Php.1.00", 197),
+("Creamy White", "Powedered Drink", "Php.15.00", 64),
+("Nescafe 3 in 1", "Powedered Drink", "Php.14", 59),
+("Pancit Canton", "Noodles", "Php.16.00", 99),
+("Cream Stick", "Snack", "Php.1.00", 8),
+("Steelwool", "Cleaning", "Php.20.00", 3),
+("President White", "Cigarette", "Php.80.00", 2),
+("Sugar 1/4", "Condiments", "Php.25.00", 16),
+("Alfonso", "Alcohol", "Php.375.00", 983),
+("Coca-Cola Mismo", "Softdrinks", "Php.20.00", 30),
+("Royal 1.5", "Softdrinks", "Php.75.00", 29);
+
+select * from sarisaristore
